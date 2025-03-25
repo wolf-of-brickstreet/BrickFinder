9c5ba59f45092a226afdfce8e7428ff669bda54b
wolmic <michael.wolf@orbis.de>
2025-03-25 09:14:21 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TestComponent from './Components/TestComponent.js'
+
+function App() {
+  return (
+    <div className="App">
+      Hello React!
+      <TestComponent name="wolmic"/>
+    </div>
+  );
+}
+
+export default App;
