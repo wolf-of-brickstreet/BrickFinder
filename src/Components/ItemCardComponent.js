664f2619fc4cf223b6fed7a4fece19fe85a3ff00
Wolf <wolf.brickstreet@gmail.com>
2025-06-21 21:48:26 +0200

    card styling

--- src/Components/ItemCardComponent.js
+++ src/Components/ItemCardComponent.js
@@ -4,7 +4,7 @@ export default function ItemCardComponent(props){
 
     return (
         <div className="card">
-            <span>{props.item.name}</span>
+            <span className="nameField">{props.item.name}</span>
             <span> {props.item.color}</span>
             <img src={props.item.image} alt={props.item.name + " in " + props.item.color} />
         </div>
