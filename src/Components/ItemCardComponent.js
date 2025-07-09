80a799671b79034ed2d812be451940bd01c60fa0
wolmic <michael.wolf@orbis.de>
2025-07-09 15:30:54 +0200

    delete button for itemcard

--- src/Components/ItemCardComponent.js
+++ src/Components/ItemCardComponent.js
@@ -1,9 +1,24 @@
 import './ItemCardStyles.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
 export default function ItemCardComponent(props){
+    async function deleteItem() {
+        await fetch('http://localhost:3001/delete-item', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(props.item)
+        });
+    }
 
     return (
         <div className="card">
+            <div className="closeButtonContainer">
+                <FontAwesomeIcon icon={faXmark} onClick={() =>{deleteItem()} } className="closeButton"/>
+            </div>
             <span className="nameField">{props.item.name}</span>
             <span> {props.item.color}</span>
             <img src={props.item.image} alt={props.item.name + " in " + props.item.color} />
